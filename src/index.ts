cf1f2b37c64e986562af7f41a97a09fe0d838d11
Ourai Lin <ourairyu@gmail.com>
2021-12-18 11:30:30 +0800

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import './presets';
 
+import { ComponentCtor, ComponentDescriptor } from './types/component';
+
 export * from '@handie/runtime-core';
+
+export * from './utils';
+
+export { ComponentCtor, ComponentDescriptor };
