13b602172dbb7b4f7b36a8657a14e5eab9c9d5bc
Ourai Lin <ourairyu@gmail.com>
2022-01-28 16:07:13 +0800

    feat: common logic of `DateFilterStructuralWidget`

--- src/widgets/class/filter/DateFilter.tsx
+++ src/widgets/class/filter/DateFilter.tsx
@@ -4,9 +4,38 @@ import { DateFilterHeadlessWidget } from '@handie/runtime-core/dist/widgets';
 import { FilterStructuralWidget } from './Filter';
 
 class DateFilterStructuralWidget<
+  VT extends DateValue | DateValue[],
   S extends DateFilterWidgetState = DateFilterWidgetState,
   CT extends DateFilterWidgetConfig = DateFilterWidgetConfig
-> extends FilterStructuralWidget<DateValue, CT, DateFilterHeadlessWidget<CT>, S> {
+> extends FilterStructuralWidget<VT, CT, DateFilterHeadlessWidget<VT, CT>, S> {
+  protected formatDate(value: DateValue): DateValue {
+    return this.$$_h.formatDate(value);
+  }
+
+  protected getSeparator(): string {
+    return this.config.separator || '-';
+  }
+
+  protected getRangeValue(): DateValue[] {
+    return this.$$_h.getRangeValue();
+  }
+
+  protected getRangePlaceholders(): string[] {
+    return this.$$_h.getRangePlaceholders();
+  }
+
+  protected onRangeChange(dates: (Date | null)[] | null): void {
+    const { fromField, toField } = this.config;
+    const range = dates ? dates.map((date: Date | null) => (date ? date.getTime() : '')) : [];
+
+    if (!fromField && !toField) {
+      this.onChange(range as VT);
+    }
+
+    this.$$search.setFilterValue(fromField!, range[0]);
+    this.$$search.setFilterValue(toField!, range[1]);
+  }
+
   public componentWillMount(): void {
     this.setHeadlessWidget(new DateFilterHeadlessWidget(this.props, this.$$view));
   }
